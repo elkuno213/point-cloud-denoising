208d5591360e8b06a3773972b2c58b88d33ab94c
huynhvuh <55743792+huynhvuh@users.noreply.github.com>
2021-06-14 10:45:42 +0200

    New template of CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,69 @@
 #include <iostream>
 #include <vector>
+#include <sys/stat.h>
 #include "rapidcsv.h"
 #include "CSVReader.hpp"
 #include "SphericalProjection.hpp"
 #include "ImageDenoising.hpp"
 #include <opencv2/opencv.hpp>
 
-void displayMinMaxLoc(cv::Mat& image)
-{
-    double minVal, maxVal; 
-    cv::Point minLoc, maxLoc;
-    cv::minMaxLoc(image, &minVal, &maxVal, &minLoc, &maxLoc);
-    std::cout   << "(minVal, maxVal, minLoc, maxLoc):\t(" 
-                << minVal << "; \t"
-                << maxVal << "; \t"
-                << minLoc << "; \t"
-                << maxLoc << ")"
-                << std::endl;
-}
+void createDir(std::string dirPath);
+
+void denoiseImageExmaple(int argc, char* argv[]);
+
+cv::Mat addNoise(cv::Mat image, double mean, double stdDev);
+
+void displayMinMaxLoc(cv::Mat& image);
 
 template<typename T>
-void display2DVector(std::vector<std::vector<T>> vector)
-{
-    for (const auto &row : vector) {
-        for (const auto &s : row) 
-            std::cout << s << ' ';
-        std::cout << std::endl;
-    }
-}
+void display2DVector(std::vector<std::vector<T>> vector);
 
 int main(int argc, char* argv[])
 {
-    // ------------------------------------------------------------------------------------------------------------
     // Parameters
 	int height 			    = 32;		// pixel
-	int width 			    = 1000;		// pixel
+	int width 			    = 500;		// pixel
 	double elevation_max 	= 15.0;		// degree
 	double elevation_min 	= -16.0;	// degree
 	double delta_elevation 	= 1.0;		// degree
 	double azimuth_max 		= 360.0;	// degree
 	double azimuth_min 		= 0.0;		// degree
 	double delta_azimuth 	= 0.18;		// degree
-    std::string nameLIDAR = argv[1];
-    std::string nameCase = std::to_string(delta_azimuth).substr(0, 4) + "_" + std::to_string(height) + "x" + std::to_string(width);
+
+    // Input paths
+    // argv[1]: Log name (Log_05s, Log_30s, Log_2min)
+    // argv[2]: LIDAR name (Back, Front)
+    std::string dataPath = "/home/hvh/MyGit/PointCloud_Denoising/data/Logs/" + std::string(argv[1]) + "/" + std::string(argv[2]);
+
+    // Output paths
+    std::string outputDir = std::to_string(delta_azimuth).substr(0, 4) + "_" + std::to_string(height) + "x" + std::to_string(width);
+    std::string outputPath = "/home/hvh/MyGit/PointCloud_Denoising/data/Output/" + std::string(argv[1]) + "/" + std::string(argv[2]) + "/" + outputDir + "/";
+    createDir(outputPath);
+
+    std::string distancePath = outputPath + "distance/";
+    createDir(distancePath);
+
+    std::string denoisedDistancePath = outputPath + "denoisedDistance/";
+    createDir(denoisedDistancePath);
+
+    std::string distanceNoisePath = outputPath + "distanceNoise/";
+    createDir(distanceNoisePath);
+
+    std::string intensityPath = outputPath + "intensity/";
+    createDir(intensityPath);
+
+    std::string denoisedIntensityPath = outputPath + "denoisedIntensity/";
+    createDir(denoisedIntensityPath);
+
+    std::string intensityNoisePath = outputPath + "intensityNoise/";
+    createDir(intensityNoisePath);
+    
 
     // Azimuth reading
     CSVReader azimuthReader;
     std::vector<std::vector<double>> azimuthData;
     azimuthReader.setParameters(0.01, -1, 0);
-    azimuthReader.readInputs("/home/hvh/MyGit/PointCloud_Denoising/data/Logs/Log_2min/" + nameLIDAR + "Azimuths.csv");
+    azimuthReader.readInputs(dataPath + "Azimuths.csv");
     azimuthReader.processData();
     azimuthReader.writeOutputs(azimuthData);
     std::cout << "azimuthData:\t(" << azimuthData.size() << " rows, " << azimuthData[0].size() << " cols)" << std::endl;
@@ -57,7 +72,7 @@ int main(int argc, char* argv[])
     CSVReader distanceReader;
     std::vector<std::vector<double>> distanceData;
     distanceReader.setParameters(0.004, -1, 0);
-    distanceReader.readInputs("/home/hvh/MyGit/PointCloud_Denoising/data/Logs/Log_2min/" + nameLIDAR + "Distances.csv");
+    distanceReader.readInputs(dataPath + "Distances.csv");
     distanceReader.processData();
     distanceReader.writeOutputs(distanceData);
     std::cout << "distanceData:\t(" << distanceData.size() << " rows, " << distanceData[0].size() << " cols)" << std::endl;
@@ -66,23 +81,24 @@ int main(int argc, char* argv[])
     CSVReader intensityReader;
     std::vector<std::vector<double>> intensityData;
     intensityReader.setParameters(1, -1, 0);
-    intensityReader.readInputs("/home/hvh/MyGit/PointCloud_Denoising/data/Logs/Log_2min/" + nameLIDAR + "Intensities.csv");
+    intensityReader.readInputs(dataPath + "Intensities.csv");
     intensityReader.processData();
     intensityReader.writeOutputs(intensityData);
     std::cout << "intensityData:\t(" << intensityData.size() << " rows, " << intensityData[0].size() << " cols)" << std::endl;
 
     // Video objects
-    cv::VideoWriter videoDistance(  "/home/hvh/MyGit/PointCloud_Denoising/data/" + nameLIDAR + "/" + nameCase + "/Distance.avi", 
+    cv::VideoWriter videoDistance(outputPath + "Distance.avi", 
                                     cv::VideoWriter::fourcc('M','J','P','G'), 10, cv::Size(width, height), false);
-    cv::VideoWriter videoDenoisedDistance(  "/home/hvh/MyGit/PointCloud_Denoising/data/" + nameLIDAR + "/" + nameCase + "/DenoisedDistance.avi", 
+    cv::VideoWriter videoDenoisedDistance(outputPath + "DenoisedDistance.avi", 
                                     cv::VideoWriter::fourcc('M','J','P','G'), 10, cv::Size(width, height), false);
-    cv::VideoWriter videoIntensity(  "/home/hvh/MyGit/PointCloud_Denoising/data/" + nameLIDAR + "/" + nameCase + "/Intensity.avi", 
+    cv::VideoWriter videoIntensity(outputPath + "Intensity.avi", 
                                     cv::VideoWriter::fourcc('M','J','P','G'), 10, cv::Size(width, height), false);
-    cv::VideoWriter videoDenoisedIntensity(  "/home/hvh/MyGit/PointCloud_Denoising/data/" + nameLIDAR + "/" + nameCase + "/DenoisedIntensity.avi", 
+    cv::VideoWriter videoDenoisedIntensity(outputPath + "DenoisedIntensity.avi", 
                                     cv::VideoWriter::fourcc('M','J','P','G'), 10, cv::Size(width, height), false);
                                 
-    // Spherical projection 
+    // PointCloud Denosing
     for (int i {0}; i < azimuthData.size(); ++i) {
+        // Spherical projection 
         cv::Mat oImage;
         SphericalProjection sp;
         sp.setParameters(height, width, elevation_max, elevation_min, delta_elevation, azimuth_max, azimuth_min, delta_azimuth);
@@ -110,7 +126,7 @@ int main(int argc, char* argv[])
         // cv::subtract(cv::Scalar::all(255.0), distanceImage, distanceImage);
         distanceImage.convertTo(distanceImage, CV_8UC1);
         videoDistance.write(distanceImage);
-        cv::imwrite("/home/hvh/MyGit/PointCloud_Denoising/data/" + nameLIDAR + "/" + nameCase + "/distanceImages/distanceImage" + std::to_string(i) + ".png", distanceImage);
+        cv::imwrite(distancePath + "distance" + std::to_string(i) + ".png", distanceImage);
 
         // Denoise distance image
         cv::Mat denoisedDistanceImage, distanceNoise;
@@ -120,16 +136,15 @@ int main(int argc, char* argv[])
         idDistance.readInputs(distanceImage);
         idDistance.processData();
         idDistance.writeOutputs(denoisedDistanceImage, distanceNoise, distanceNoiseRatio);
-        // std::cout << "distanceNoiseRatio:\t" << distanceNoiseRatio << std::endl;
         videoDenoisedDistance.write(denoisedDistanceImage);
-        cv::imwrite("/home/hvh/MyGit/PointCloud_Denoising/data/" + nameLIDAR + "/" + nameCase + "/denoisedDistanceImages/denoisedDistanceImage" + std::to_string(i) + ".png", denoisedDistanceImage);
-        cv::imwrite("/home/hvh/MyGit/PointCloud_Denoising/data/" + nameLIDAR + "/" + nameCase + "/distanceNoises/distanceNoise" + std::to_string(i) + ".png", distanceNoise);
+        cv::imwrite(denoisedDistancePath + "denoisedDistance" + std::to_string(i) + ".png", denoisedDistanceImage);
+        cv::imwrite(distanceNoisePath + "distanceNoise" + std::to_string(i) + ".png", distanceNoise);
 
         // Intensity image
         cv::Mat intensityImage = channels[3];
         intensityImage.convertTo(intensityImage, CV_8UC1);
         videoIntensity.write(intensityImage);
-        cv::imwrite("/home/hvh/MyGit/PointCloud_Denoising/data/" + nameLIDAR + "/" + nameCase + "/intensityImages/intensityImage" + std::to_string(i) + ".png", intensityImage);
+        cv::imwrite(intensityPath + "intensityImage" + std::to_string(i) + ".png", intensityImage);
     
         // Denoise intensity image
         cv::Mat denoisedIntensityImage, intensityNoise;
@@ -141,8 +156,8 @@ int main(int argc, char* argv[])
         idIntensity.writeOutputs(denoisedIntensityImage, intensityNoise, intensityNoiseRatio);
         // std::cout << "intensityNoiseRatio:\t" << intensityNoiseRatio << std::endl;
         videoDenoisedIntensity.write(denoisedIntensityImage);
-        cv::imwrite("/home/hvh/MyGit/PointCloud_Denoising/data/" + nameLIDAR + "/" + nameCase + "/denoisedIntensityImages/denoisedIntensityImage" + std::to_string(i) + ".png", denoisedIntensityImage);
-        cv::imwrite("/home/hvh/MyGit/PointCloud_Denoising/data/" + nameLIDAR + "/" + nameCase + "/intensityNoises/intensityNoise" + std::to_string(i) + ".png", intensityNoise);
+        cv::imwrite(denoisedIntensityPath + "denoisedIntensityImage" + std::to_string(i) + ".png", denoisedIntensityImage);
+        cv::imwrite(intensityNoisePath + "intensityNoise" + std::to_string(i) + ".png", intensityNoise);
     }
 
     videoDistance.release();
@@ -151,4 +166,60 @@ int main(int argc, char* argv[])
     videoDenoisedIntensity.release();
                          
     return 0;
+}
+
+void createDir(std::string dirPath)
+{
+    if (mkdir(dirPath.c_str(), 0777) == -1)
+        std::cerr << "Error :  " << strerror(errno) << std::endl;
+    else
+        std::cout << "Directory created" << std::endl;
+}
+
+void denoiseImageExmaple(int argc, char* argv[]) 
+{
+    cv::Mat image = cv::imread("/home/hvh/MyGit/PointCloud_Denoising/data/Back/0.18_32x1000/intensityImages/intensityImage" + std::to_string(atoi(argv[5])) + ".png", cv::IMREAD_GRAYSCALE);
+    // image.convertTo(image, CV_64F);
+    cv::Mat denoisedImage, noise;
+    double noiseRatio;
+    ImageDenoising id;
+    id.setParameters(strtod(argv[1], NULL), atoi(argv[2]), atoi(argv[3]), strtod(argv[4], NULL));
+    id.readInputs(image);
+    id.processData();
+    id.writeOutputs(denoisedImage, noise, noiseRatio);
+    cv::imwrite("/home/hvh/MyGit/PointCloud_Denoising/data/denoisedImage.png", denoisedImage);
+    cv::imwrite("/home/hvh/MyGit/PointCloud_Denoising/data/noise.png", noise);
+    std::cout << "noiseRatio:\t" << noiseRatio << std::endl;
+}
+
+cv::Mat addNoise(cv::Mat image, double mean = 0.0, double stdDev = 1.0) 
+{
+    cv::Mat noise = cv::Mat(image.size(), image.depth());
+    cv::randn(noise, cv::Scalar::all(mean), cv::Scalar::all(stdDev));
+    cv::Mat noisyImage;
+    cv::addWeighted(image, 0.5, noise, 0.5, 0.0, noisyImage);
+    return noisyImage;
+}
+
+void displayMinMaxLoc(cv::Mat& image)
+{
+    double minVal, maxVal; 
+    cv::Point minLoc, maxLoc;
+    cv::minMaxLoc(image, &minVal, &maxVal, &minLoc, &maxLoc);
+    std::cout   << "(minVal, maxVal, minLoc, maxLoc):\t(" 
+                << minVal << "; \t"
+                << maxVal << "; \t"
+                << minLoc << "; \t"
+                << maxLoc << ")"
+                << std::endl;
+}
+
+template<typename T>
+void display2DVector(std::vector<std::vector<T>> vector)
+{
+    for (const auto &row : vector) {
+        for (const auto &s : row) 
+            std::cout << s << ' ';
+        std::cout << std::endl;
+    }
 }
